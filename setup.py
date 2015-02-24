a8e72972b61d082af87e6764da23ed1ffe394341
T.J. Trimble <trimblet@me.com>
2015-02-23 20:41:21 -0800

    derivationtree.py and associated tests for storing ACE derivation trees in memory.

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     author='Michael Wayne Goodman',
     author_email='goodman.m.w@gmail.com',
     description='Libraries and scripts for DELPH-IN data.',
-    packages=['delphin','delphin.interfaces','delphin.mrs','delphin.extra','delphin.codecs'],
+    packages=['delphin','delphin.interfaces','delphin.mrs','delphin.extra','delphin.codecs', 'delphin.derivations'],
     cmdclass={'test':TestCommand},
     requires=[
         'networkx',
